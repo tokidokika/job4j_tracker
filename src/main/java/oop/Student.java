f680b4f79b36f8e3accd9fce733ddc72b7f8e668
Kate Sm <yukaicat@gmail.com>
2021-11-03 13:48:21 +0300

    1.3. Вызов метода с аргументами. [#166086]

--- src/main/java/oop/Student.java
+++ src/main/java/oop/Student.java
@@ -2,21 +2,13 @@ package oop;
 
 public class Student {
 
-    public void music() {
-        System.out.println("Tra tra tra");
-    }
-
-    public void song() {
-        System.out.println("I believe I can fly");
+    public void music(String lyrics) {
+        System.out.println("I can sign a song: " + lyrics);
     }
 
     public static void main(String[] args) {
         Student petya = new Student();
-        petya.music();
-        petya.music();
-        petya.music();
-        petya.song();
-        petya.song();
-        petya.song();
+        String song = "I believe I can fly";
+        petya.music(song);
     }
 }
