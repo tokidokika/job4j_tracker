90651714550563397e0fadeb5a5767667127e843
Kate Sm <yukaicat@gmail.com>
2021-11-14 13:34:56 +0300

    Исправление теста

--- src/test/java/ru/job4j/oop/PointTest.java
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -15,10 +15,10 @@ public class PointTest {
     }
 
     @Test
-    public void when20to00then2() {
-        double expected = 2;
-        Point a = new Point(0, 0);
-        Point b = new Point(2, 0);
+    public void when10to15then5() {
+        double expected = 5;
+        Point a = new Point(1, 0);
+        Point b = new Point(1, 5);
         double rsl = a.distance(b);
         Assert.assertEquals(expected, rsl, 0.01);
     }
