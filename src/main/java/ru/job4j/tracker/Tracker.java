713fcbb5ff46030972bde592127e564930f9254e
Kate Sm <yukaicat@gmail.com>
2022-01-23 17:40:03 +0300

    5. Tracker - хранилище [#396] исправление

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,15 +14,7 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] itemsRsl = new Item[size];
-        int count = 0;
-        for (int i = 0; i < size; i++) {
-            if (items[i] != null) {
-                itemsRsl[count] = items[i];
-                count++;
-            }
-        }
-        return Arrays.copyOf(itemsRsl, count);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
