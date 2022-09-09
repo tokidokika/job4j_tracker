379b74fff535ffd83874a99efb71506486d5a0bf
Kate Sm <yukaicat@gmail.com>
2022-09-09 22:36:54 +0300

    1. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,22 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person rsl : persons) {
+            if (rsl.getSurname().contains(key) || rsl.getPhone().contains(key) || rsl.getAddress().contains(key)
+            || rsl.getName().contains(key)) {
+                result.add(rsl);
+            }
+        }
+        return result;
+    }
+}
